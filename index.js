6388e16dccb3158842c88d846a3d3c6b15aaa1df
Kyle Mwendwa <kklegendofgaming@gmail.com>
2025-03-15 12:49:25 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+let currentUser = "Kyle";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const firstInitial = currentUser[0];
+
+const shortGreeting = `Welcome, ${firstInitial} !`;
